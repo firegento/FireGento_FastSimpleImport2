62db4c25f2b8bf501ac4a8dbc742498ba9553a6a
Elias Kotlyar <elias.kotlyar@gmail.com>
2016-01-23 03:09:50 -0800

    Corrected Extension Key

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'FireGento_FastSimpleImport',
+    'FireGento_FastSimpleImport2',
     __DIR__
 );
